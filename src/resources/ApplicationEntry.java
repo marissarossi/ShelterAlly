80e72b66459f351b257e1dadfe5d097b13bbd9eb
marissarossi <55842492+marissarossi@users.noreply.github.com>
2022-12-08 08:57:39 -0500

    adding code to classes

---
+++ src/resources/ApplicationEntry.java
@@ -0,0 +1,28 @@
+package resources;
+
+import javax.swing.*;
+
+public class ApplicationEntry {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JRadioButton lowRadioButton;
+    private JRadioButton mediumRadioButton;
+    private JRadioButton highRadioButton;
+    private JRadioButton yesRadioButton;
+    private JRadioButton noRadioButton;
+    private JRadioButton yesRadioButton1;
+    private JRadioButton noRadioButton1;
+    private JRadioButton yesRadioButton2;
+    private JRadioButton noRadioButton2;
+    private JRadioButton smallRadioButton1;
+    private JRadioButton mediumRadioButton1;
+    private JRadioButton largeRadioButton1;
+    private JRadioButton yesRadioButton3;
+    private JRadioButton noRadioButton3;
+    private JPanel MainJPanel;
+    private JPanel OutermostJpanel;
+    private JPanel BodyJPanel;
+    private JPanel QuestionsJPanel;
+    private JPanel RadioButtJPanel;
+}
