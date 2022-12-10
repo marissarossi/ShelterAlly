81370ed5e466af67a26f9290b494711a0387fc16
marissarossi <55842492+marissarossi@users.noreply.github.com>
2022-12-09 20:42:56 -0500

    Added Pending App icon and NewDogEntry GUI

---
+++ src/NewDogEntry.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class NewDogEntry {
+    private JPanel titlePanel;
+    private JPanel midPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JRadioButton smallRadioButton;
+    private JRadioButton mediumRadioButton;
+    private JRadioButton largeRadioButton;
+    private JCheckBox kidsOKCheckBox;
+    private JCheckBox otherDogsOKCheckBox;
+    private JCheckBox catsOKCheckBox;
+    private JCheckBox needsYardCheckBox;
+    private JPanel bottomPanel;
+    private JRadioButton lowRadioButton;
+    private JRadioButton mediumRadioButton1;
+    private JRadioButton highRadioButton;
+    private JRadioButton shyRadioButton;
+    private JRadioButton averageEasyGoingRadioButton;
+    private JRadioButton aggressiveRadioButton;
+}
