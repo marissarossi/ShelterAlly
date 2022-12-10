9d80e9d38799a5da1ff00db983effe534cd10b60
erinrogers15 <71192811+erinrogers15@users.noreply.github.com>
2022-12-10 10:18:46 -0500

    Added submit button to application entry form and coded form to create new application when submit button is pressed

--- src/ApplicationEntry.java
+++ src/ApplicationEntry.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ApplicationEntry extends JFrame{
     public JPanel MainJPanel;
@@ -25,4 +27,82 @@ public class ApplicationEntry extends JFrame{
     private JPanel BodyJPanel;
     private JPanel QuestionsJPanel;
     private JPanel RadioButtJPanel;
+    private JButton submitButton;
+    ButtonGroup buttonGroup1, buttonGroup2, buttonGroup3, buttonGroup4, buttonGroup5, buttonGroup6;
+
+    String name;
+    String address;
+    String phoneNumber;
+    String size;
+    boolean kidsInHome;
+    boolean otherDogsInHome;
+    boolean catsInHome;
+    String energyLevel;
+    boolean hasYard;
+
+    public ApplicationEntry() {
+        // size radio buttons
+        smallRadioButton1.setActionCommand("small");
+        mediumRadioButton1.setActionCommand("medium");
+        largeRadioButton1.setActionCommand("large");
+        // energy level radio buttons
+        lowRadioButton.setActionCommand("low");
+        mediumRadioButton.setActionCommand("medium");
+        highRadioButton.setActionCommand("high");
+
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                name = textField1.getText();
+                address = textField2.getText();
+                phoneNumber = textField3.getText();
+                size = buttonGroup1.getSelection().getActionCommand();
+                energyLevel = buttonGroup2.getSelection().getActionCommand();
+
+                if (yesRadioButton.isSelected()){
+                    kidsInHome = true;
+                }
+                else if (noRadioButton.isSelected()){
+                    kidsInHome = false;
+                }
+
+
+                if (yesRadioButton1.isSelected()){
+                    otherDogsInHome = true;
+                }
+                else if (noRadioButton1.isSelected()){
+                    otherDogsInHome = false;
+                }
+
+                if (yesRadioButton2.isSelected()){
+                    catsInHome = true;
+                }
+                else if (noRadioButton2.isSelected()){
+                    catsInHome = false;
+                }
+
+                if (yesRadioButton3.isSelected()){
+                    hasYard = true;
+                }
+                else if (noRadioButton3.isSelected()){
+                    hasYard = false;
+                }
+                // for testing quick
+                Application app = new Application();
+                app.setName(name);
+                app.setAddress(address);
+                app.setPhoneNumber(phoneNumber);
+                app.setDogSize(size);
+                app.setEnergyLevel(energyLevel);
+                app.setOtherDogs(otherDogsInHome);
+                app.setHasCats(catsInHome);
+                app.setKidsInHome(kidsInHome);
+                app.setHasYard(hasYard);
+
+                // to keep Added Dogs in the list in "Current Dogs", keep the window open
+                PendingApplications.getModel().addRow(new Object[]{name, address, phoneNumber, size, energyLevel, otherDogsInHome, catsInHome, kidsInHome, hasYard});
+            }
+        });
+    }
 }
