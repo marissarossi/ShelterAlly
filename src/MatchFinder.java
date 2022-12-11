9e6b7cd46e21bec0ceaef88555573299b1a10cc0
marissarossi <55842492+marissarossi@users.noreply.github.com>
2022-12-10 19:58:12 -0500

    MatchFinder GUI is good now

--- src/MatchFinder.java
+++ src/MatchFinder.java
@@ -1,21 +1,23 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class MatchFinder extends JFrame {
-    JTextField matchFoundTextField;
-    JTextField dogNameTextField;
     JButton scheduleMeetAndGreetButton;
     JPanel MatchFoundJPanel;
     JTextField textField1;
 
     public MatchFinder(){
-
+        super.setSize(600,400);
         scheduleMeetAndGreetButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
             }
         });
+
     }
 }
